7b9727def7a47d276f8c143876453db67be643a8
Ingsay <ingsay177@gmail.com>
2023-11-16 16:49:47 +0100

    Gotowe

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+  
+    let interval = null; // Zmienna do przechowywania identyfikatora interwału
+  
+    startButton.addEventListener('click', () => {
 
+    startButton.disabled = true;
+    
+    interval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+  
+    stopButton.addEventListener('click', () => {
+
+    startButton.disabled = false;
+    
+    clearInterval(interval);
+  });
